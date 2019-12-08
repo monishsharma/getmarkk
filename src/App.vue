5b726629285e00465652e61f6e1479b865b6c6a0
Monish sharma <sharma.monish17@gmail.com>
2019-12-08 17:24:14 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <Maps />
+    <Card />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/UI/NavBar/NavBar.vue";
+import Maps from "./components/UI/Maps/Maps.vue";
+import Card from "./components/UI/Card/Card.vue";
+import Footer from "./components/UI/Footer/Footer.vue";
+export default {
+  name: "app",
+  components: {
+    Navbar,
+    Maps,
+    Card,
+    Footer
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: black;
+}
+#app {
+  overflow-x: hidden;
+
+}
+</style>
